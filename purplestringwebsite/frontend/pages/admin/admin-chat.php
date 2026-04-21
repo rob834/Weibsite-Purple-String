2d9733272bdf91d4ab41af06947cd85dead75b35
Louise Abraham V. Manio <98595988+rui-ise@users.noreply.github.com>
2026-04-21 16:22:16 +0800

    minor fix

--- purplestringwebsite/frontend/pages/admin/admin-chat.php
+++ purplestringwebsite/frontend/pages/admin/admin-chat.php
@@ -6,6 +6,8 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- purplestringwebsite/frontend/pages/admin/admin-products-edit.php
+++ purplestringwebsite/frontend/pages/admin/admin-products-edit.php
@@ -6,6 +6,9 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
